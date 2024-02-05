345d7c07fefac5e3a44609c173e0109a6637b864
Ditto-N <68272497+Ditto-N@users.noreply.github.com>
2024-02-05 17:51:32 +0800

    更新服务器搭建手册

--- star_front/src/App.vue
+++ star_front/src/App.vue
@@ -1,26 +1,11 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
+<script setup>
 import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+import IfDemo from "./components/IfDemo.vue"
+import StarTools from './components/StarTools.vue';
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <HelloWorld/>
+  <IfDemo/>
+  <StarTools/>
+  </template>
